0f3b27ba01b6ada94503104864daedecbde084db
idrisoff <idrisoff1986@mail.ru>
2021-12-27 06:41:34 +0500

    test
    test

---
+++ Form1.Designer.cs
@@ -0,0 +1,333 @@
+﻿
+namespace dataset
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxLastName = new System.Windows.Forms.TextBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxFather = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxPhone = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxAdress = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxBirthday = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.LoadImage = new System.Windows.Forms.Button();
+            this.DeleteImage = new System.Windows.Forms.Button();
+            this.SaveAll = new System.Windows.Forms.Button();
+            this.LoadAll = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Add = new System.Windows.Forms.Button();
+            this.Update = new System.Windows.Forms.Button();
+            this.Delete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(28, 25);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(644, 375);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(736, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Фамилия";
+            // 
+            // textBoxLastName
+            // 
+            this.textBoxLastName.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxLastName.Location = new System.Drawing.Point(842, 25);
+            this.textBoxLastName.Name = "textBoxLastName";
+            this.textBoxLastName.Size = new System.Drawing.Size(216, 25);
+            this.textBoxLastName.TabIndex = 2;
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxName.Location = new System.Drawing.Point(842, 70);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(216, 25);
+            this.textBoxName.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(736, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Имя";
+            // 
+            // textBoxFather
+            // 
+            this.textBoxFather.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxFather.Location = new System.Drawing.Point(842, 118);
+            this.textBoxFather.Name = "textBoxFather";
+            this.textBoxFather.Size = new System.Drawing.Size(216, 25);
+            this.textBoxFather.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(736, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Отчество";
+            // 
+            // textBoxPhone
+            // 
+            this.textBoxPhone.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxPhone.Location = new System.Drawing.Point(842, 168);
+            this.textBoxPhone.Name = "textBoxPhone";
+            this.textBoxPhone.Size = new System.Drawing.Size(216, 25);
+            this.textBoxPhone.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(736, 171);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Телефон";
+            // 
+            // textBoxAdress
+            // 
+            this.textBoxAdress.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxAdress.Location = new System.Drawing.Point(842, 217);
+            this.textBoxAdress.Name = "textBoxAdress";
+            this.textBoxAdress.Size = new System.Drawing.Size(216, 25);
+            this.textBoxAdress.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(736, 220);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Адрес";
+            // 
+            // textBoxBirthday
+            // 
+            this.textBoxBirthday.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxBirthday.Location = new System.Drawing.Point(842, 270);
+            this.textBoxBirthday.Name = "textBoxBirthday";
+            this.textBoxBirthday.Size = new System.Drawing.Size(216, 25);
+            this.textBoxBirthday.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(736, 273);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(100, 17);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Дата рождения";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(1125, 33);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(38, 17);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Фото";
+            // 
+            // LoadImage
+            // 
+            this.LoadImage.Location = new System.Drawing.Point(1216, 73);
+            this.LoadImage.Name = "LoadImage";
+            this.LoadImage.Size = new System.Drawing.Size(75, 23);
+            this.LoadImage.TabIndex = 15;
+            this.LoadImage.Text = "Загрузить";
+            this.LoadImage.UseVisualStyleBackColor = true;
+            this.LoadImage.Click += new System.EventHandler(this.LoadImage_Click);
+            // 
+            // DeleteImage
+            // 
+            this.DeleteImage.Location = new System.Drawing.Point(1217, 102);
+            this.DeleteImage.Name = "DeleteImage";
+            this.DeleteImage.Size = new System.Drawing.Size(75, 23);
+            this.DeleteImage.TabIndex = 16;
+            this.DeleteImage.Text = "Удалить";
+            this.DeleteImage.UseVisualStyleBackColor = true;
+            this.DeleteImage.Click += new System.EventHandler(this.DeleteImage_Click);
+            // 
+            // SaveAll
+            // 
+            this.SaveAll.Location = new System.Drawing.Point(771, 481);
+            this.SaveAll.Name = "SaveAll";
+            this.SaveAll.Size = new System.Drawing.Size(240, 23);
+            this.SaveAll.TabIndex = 17;
+            this.SaveAll.Text = "Сохранить справочник";
+            this.SaveAll.UseVisualStyleBackColor = true;
+            this.SaveAll.Click += new System.EventHandler(this.SaveAll_Click);
+            // 
+            // LoadAll
+            // 
+            this.LoadAll.Location = new System.Drawing.Point(1063, 481);
+            this.LoadAll.Name = "LoadAll";
+            this.LoadAll.Size = new System.Drawing.Size(228, 23);
+            this.LoadAll.TabIndex = 18;
+            this.LoadAll.Text = "Загрузить справочник";
+            this.LoadAll.UseVisualStyleBackColor = true;
+            this.LoadAll.Click += new System.EventHandler(this.LoadAll_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = global::dataset.Properties.Resources.FemaleDemo;
+            this.pictureBox1.Image = global::dataset.Properties.Resources.FemaleDemo;
+            this.pictureBox1.InitialImage = global::dataset.Properties.Resources.FemaleDemo;
+            this.pictureBox1.Location = new System.Drawing.Point(1081, 70);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(130, 123);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.WaitOnLoad = true;
+            // 
+            // Add
+            // 
+            this.Add.Location = new System.Drawing.Point(28, 481);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(60, 23);
+            this.Add.TabIndex = 19;
+            this.Add.Text = "Add";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // Update
+            // 
+            this.Update.Location = new System.Drawing.Point(111, 481);
+            this.Update.Name = "Update";
+            this.Update.Size = new System.Drawing.Size(70, 23);
+            this.Update.TabIndex = 20;
+            this.Update.Text = "Update";
+            this.Update.UseVisualStyleBackColor = true;
+            this.Update.Click += new System.EventHandler(this.Update_Click);
+            // 
+            // Delete
+            // 
+            this.Delete.Location = new System.Drawing.Point(205, 481);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(60, 23);
+            this.Delete.TabIndex = 21;
+            this.Delete.Text = "Delete";
+            this.Delete.UseVisualStyleBackColor = true;
+            this.Delete.Click += new System.EventHandler(this.Delete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1303, 538);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.Update);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.LoadAll);
+            this.Controls.Add(this.SaveAll);
+            this.Controls.Add(this.DeleteImage);
+            this.Controls.Add(this.LoadImage);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBoxBirthday);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBoxAdress);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxPhone);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxFather);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxLastName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxLastName;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxFather;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxPhone;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxAdress;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxBirthday;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button LoadImage;
+        private System.Windows.Forms.Button DeleteImage;
+        private System.Windows.Forms.Button SaveAll;
+        private System.Windows.Forms.Button LoadAll;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.Button Update;
+        private System.Windows.Forms.Button Delete;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dataset
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        DataTable table = new DataTable();
+        int selectedRow;
+
+        private void LoadImage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog opf = new OpenFileDialog();
+            opf.Filter = "Choose Image(*.JPG;*.PNG;*.GIF)|*.jpg;*.png;*.gif";
+
+            if (opf.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = Image.FromFile(opf.FileName);
+            }
+        }
+
+        private void DeleteImage_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Properties.Resources.FemaleDemo;
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            table.Rows.Add(textBoxLastName.Text, textBoxName.Text, textBoxFather.Text, textBoxPhone.Text, textBoxAdress.Text, textBoxBirthday.Text);
+            dataGridView1.DataSource = table;
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+            table.Columns.Add("LastName", typeof(string));
+            table.Columns.Add("Name", typeof(string));
+            table.Columns.Add("Father", typeof(string));
+            table.Columns.Add("PhoneNumber", typeof(string));
+            table.Columns.Add("Adress", typeof(string));
+            table.Columns.Add("DateOfBirth", typeof(string));
+
+            table.Rows.Add("Idrisov", "Rinat", "Gilyagetdinovich", "45u4839", "fklajsdflklds", new DateTime(1986, 7, 6).ToString());
+            dataGridView1.DataSource = table;
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            selectedRow = e.RowIndex;
+            DataGridViewRow row = dataGridView1.Rows[selectedRow];
+            textBoxLastName.Text = row.Cells[0].Value.ToString();
+            textBoxName.Text = row.Cells[1].Value.ToString();
+            textBoxFather.Text = row.Cells[2].Value.ToString();
+            textBoxPhone.Text = row.Cells[3].Value.ToString();
+            textBoxAdress.Text = row.Cells[4].Value.ToString();
+            textBoxBirthday.Text = row.Cells[5].Value.ToString();
+        }
+
+        private void Update_Click(object sender, EventArgs e)
+        {
+            DataGridViewRow newDataRow = dataGridView1.Rows[selectedRow];
+            newDataRow.Cells[0].Value = textBoxLastName.Text;
+            newDataRow.Cells[1].Value = textBoxName.Text;
+            newDataRow.Cells[2].Value = textBoxFather.Text;
+            newDataRow.Cells[3].Value = textBoxPhone.Text;
+            newDataRow.Cells[4].Value = textBoxAdress.Text;
+            newDataRow.Cells[5].Value = textBoxBirthday.Text;
+        }
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                selectedRow = dataGridView1.CurrentCell.RowIndex;
+                dataGridView1.Rows.RemoveAt(selectedRow);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private async void LoadAll_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog opf = new OpenFileDialog();
+            opf.Filter = "Choose Text(*.txt)|*.txt";
+
+            if (opf.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamReader sr = new StreamReader(opf.FileName, System.Text.Encoding.Default))
+                {
+                    string line;
+                    while ((line = await sr.ReadLineAsync()) != null)
+                    {
+                        var arr = line.Split('^', StringSplitOptions.RemoveEmptyEntries);
+                        try
+                        {
+                            for (int i = 0; i < arr.Length; i++)
+                            {
+                                arr[i] = arr[i].Replace("[etot_simvol]", "^");
+                            }
+                            table.Rows.Add(arr[0], arr[1], arr[2], arr[3], arr[4], arr[5]);
+                        }
+                        catch (Exception ex)
+                        {
+                            return;
+                        }
+                    }
+                    dataGridView1.DataSource = table;
+                }
+            }
+        }
+
+        private async void SaveAll_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfp = new SaveFileDialog();
+            Stream myStream;
+            if (sfp.ShowDialog() == DialogResult.OK)
+            {
+                if ((myStream = sfp.OpenFile()) != null)
+                {
+                    using (StreamWriter sw = new StreamWriter(myStream, Encoding.Default))
+                    {
+                        try
+                        {
+                            for (int i = 0; i < dataGridView1.RowCount - 1; i++)
+                            {
+                                for (int j = 0; j < dataGridView1.ColumnCount; j++)
+                                {
+                                    string data = dataGridView1.Rows[i].Cells[j].Value.ToString().Replace("^", "[etot_simvol]");
+                                    await sw.WriteAsync(data + "^");
+                                }
+                                await sw.WriteLineAsync();
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
